49c6117ee5e162f2fcac1c1154a58845dd3a1087
giulia notaro <notarogiulia10@gmail.com>
2021-12-13 19:47:50 +0100

    feat: add header/footer

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import styles from "./Header.module.scss"
+
+
+const Header = (props) => {
+    const name = props.name || "App";
+    const links = props.links|| [{ link:"", label: "Link" }];
+
+
+ 
+
+  return (
+    <header >
+      <h1>{name}</h1>
+      <nav >
+        <ul className={styles.ul}>
+
+          {links.map((item, index) => 
+            <li key={index}>
+              <a href={item.link}>{item.label}</a>
+            </li>
+          )}
+
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
