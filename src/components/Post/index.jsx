7c90295a6d654221dce9b53b9a450f36b071a037
giulia notaro <notarogiulia10@gmail.com>
2021-12-21 18:10:01 +0100

    repo aggiornata

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,22 +1,47 @@
-import { InData } from "./../inData ";
+import { formatDistance } from "date-fns";
+import { it } from "date-fns/locale";
+import { httpDELETE } from "../../libs/http";
 import styles from "./Post.module.scss";
 
 const Post = (props) => {
   const data = props.data || {
-    author: "user",
-    text: "",
+    author: "User",
+    text: "text",
     date: new Date(),
     photo:
-      "https://images.unsplash.com/photo-1577188947090-95007502b22f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1024&q=80",
+      "https://images.unsplash.com/photo-1639512398860-be15f48100ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1024&q=80",
+  };
+
+  const handleDeletePost = () => {
+    httpDELETE(`/posts/${props.data.id}`).then(() => {});
+    alert(`Post by ${props.data.author} was deleted`);
+    window.location.href = "/";
   };
 
   return (
     <article className={styles.post}>
+      <button onClick={handleDeletePost} className={styles.deleteBtn}>
+        X
+      </button>
+
       <h3>{data.author}</h3>
-      <InData data={data} />
+      <p>
+        <small>
+          {formatDistance(new Date(data.date), new Date(), {
+            addSuffix: true,
+            locale: it,
+          })}
+        </small>
+      </p>
       <p>{data.text}</p>
 
       {data.photo ? <img src={data.photo} alt={data.author} /> : <></>}
+
+      {/*
+      Per gestire condizioni più complesse posso richiamare una funzione 
+      
+      hasImage(data.photo) 
+      */}
     </article>
   );
 };
